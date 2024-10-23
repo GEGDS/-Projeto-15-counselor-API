a6745f41202e67cdfbf34cea4a5d31868256cc42
Gabriel Silva <gabrielback2009@hotmail.com>
2024-10-23 00:16:01 -0300

    v3 organizando rotas

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,23 @@ import axios from 'axios';
 const router = express.Router();
 
 
+//rota /
+router.get("/", (req,res) => {
+    res.render('index.ejs');
+});
+
+router.get('/advice', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        const advice = response.data.slip.advice;
+
+        res.render('adivice', { advice });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro in seeking advice.');
+    }
+});
+
 
 
 
